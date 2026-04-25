042f935e4ae58d506e368899f5b8b8e8bdd833a0
TommyEQ <tomasequesada@gmail.com>
2026-04-25 13:03:18 -0500

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -83,7 +83,7 @@ export default function IndexPage() {
             height={100}
             width={100}
             sizes="100vw"
-            className="bg-primary rounded-full mb-0 h-auto md:mb-2 w-[60%] max-w-[16rem] border-8 border-[#4D2F1A]"
+            className="bg-primary rounded-full mb-0 h-auto md:mb-2 w-[60%] max-w-[16rem] border-8 border-primary"
             alt="Tomas Quesada - Mechanical Engineer Portfolio"
             priority
           />
@@ -108,37 +108,37 @@ export default function IndexPage() {
           </div>
 
           <div className="flex flex-col mt-10 items-center justify-center sm:flex-row sm:space-x-4 gap-3">
-            <AnimatedText delay={0.6}>
-  <Link
-    href={siteConfig.links.linkedin}
-    target="_blank"
-    className={cn(buttonVariants({ variant: "outline", size: "lg" }), "border-[#4D2F1A] text-[#4D2F1A]")}
-    aria-label="View Tomas Quesada's LinkedIn profile"
-  >
-    <Icons.linkedin className="w-4 h-4 mr-2" /> Linkedin
-  </Link>
-</AnimatedText>
-<AnimatedText delay={0.8}>
-  <Link
-    href={"/contact"}
-    rel="noreferrer"
-    className={cn(buttonVariants({ variant: "outline", size: "lg" }), "border-[#4D2F1A] text-[#4D2F1A]")}
-    aria-label="Contact Tomas Quesada"
-  >
-    <Icons.contact className="w-4 h-4 mr-2" /> Contact
-  </Link>
-</AnimatedText>
-<AnimatedText delay={1.0}>
-  <Link
-    href="/resume.pdf"
-    target="_blank"
-    className={cn(buttonVariants({ variant: "outline", size: "lg" }), "border-[#4D2F1A] text-[#4D2F1A]")}
-    aria-label="View Tomas Quesada's Resume"
-  >
-    Resume
-  </Link>
-</AnimatedText>
-          </div>
+  <AnimatedText delay={0.6}>
+    <Link
+      href={siteConfig.links.linkedin}
+      target="_blank"
+      className={cn(buttonVariants({ variant: "outline", size: "lg" }), "border-primary text-primary")}
+      aria-label="View Tomas Quesada's LinkedIn profile"
+    >
+      <Icons.linkedin className="w-4 h-4 mr-2" /> Linkedin
+    </Link>
+  </AnimatedText>
+  <AnimatedText delay={0.8}>
+    <Link
+      href={"/contact"}
+      rel="noreferrer"
+      className={cn(buttonVariants({ variant: "outline", size: "lg" }), "border-primary text-primary")}
+      aria-label="Contact Tomas Quesada"
+    >
+      <Icons.contact className="w-4 h-4 mr-2" /> Contact
+    </Link>
+  </AnimatedText>
+  <AnimatedText delay={1.0}>
+    <Link
+      href="/resume.pdf"
+      target="_blank"
+      className={cn(buttonVariants({ variant: "outline", size: "lg" }), "border-primary text-primary")}
+      aria-label="View Tomas Quesada's Resume"
+    >
+      Resume
+    </Link>
+  </AnimatedText>
+</div>
           <AnimatedText delay={1.2}>
             <Icons.chevronDown className="h-6 w-6 mt-10" />
           </AnimatedText>
